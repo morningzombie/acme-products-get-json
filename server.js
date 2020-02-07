f88add40132c08634fdd87018d85cfec1e2a0641
morningzombie <56514455+morningzombie@users.noreply.github.com>
2020-02-07 08:23:37 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,19 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const db = require('./db')
-
 const products = require('./products')
 const port = 3000
-const react = require('react')
 
+app.use(express.static('public'));
 
 
 app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.get('/api/products', (req, res, next) => {
 
+
+app.get('/api/products', (req, res, next) => {
     res.send(products);
 });
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
