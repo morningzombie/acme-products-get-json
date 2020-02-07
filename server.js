7bd0ff4ca03b1a01136c4fd0b67c89b8f98c52ee
morningzombie <56514455+morningzombie@users.noreply.github.com>
2020-02-07 11:09:47 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const db = require('./db')
 const products = require('./products')
+const companies = require('./companies')
+
 const port = 3000
 
 app.use(express.static('public'));
@@ -17,6 +19,8 @@ app.get('/api/products', (req, res, next) => {
     res.send(products);
 });
 
-
+app.get('/api/companies', (req, res, next) => {
+    res.send(companies);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
